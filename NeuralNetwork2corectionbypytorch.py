70be2ed9441c136cd81fba4f3d6bf9d1527245d0
Jorge Canto <jorge.l.santiago@cua.uam.mx>
2026-07-16 13:29:20 -0600

    Adding Functions to the corrected version

--- NeuralNetwork2corectionbypytorch.py
+++ NeuralNetwork2corectionbypytorch.py
@@ -16,8 +16,17 @@ def g_def(u):
 def H_def(x1, x2, l, c):
     return ((1 / (2 * l)) * (x1**2) + (1 / (2 * c)) * (x2**2))
 
-def desire_functions(w, x):
+def desired_functions(w, x):
     return w + x
+
+def partial_derivate_H(l,c,x1,x2):
+    return torch.tensor([[x1,l][x2/c]], dtype=torch.float32)
+    
+def desired_x(v_desired,r,l,c):
+    return torch.tensor([[(l/r)*v_desired][c*v_desired]])
+
+def desired_u(v_in,v_desired):
+    return v_desired/v_in
 
 class HamiltonianNetwork(nn.Module):
     def __init__(self, in_data=2, out_data=3, layers=64):
